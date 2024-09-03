86546e05b3c112c23b5cd589010e3ecc08224fba
BoxFullOfBees <advdv369@gmail.com>
2024-09-02 20:53:23 -0500

    Delete Numi directory

--- Numi/obj/Debug/netstandard2.0/MyPluginInfo.cs
+++
@@ -1,9 +0,0 @@
-namespace NumiEnemy
-{
-    public static class MyPluginInfo
-    {
-        public const string PLUGIN_GUID = "NumiEnemy";
-        public const string PLUGIN_NAME = "NumiEnemy";
-        public const string PLUGIN_VERSION = "1.0.0";
-    }
-}
